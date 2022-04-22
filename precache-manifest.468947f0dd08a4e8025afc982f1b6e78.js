094c7ead813e4ba87aea85bf572152245384a8ed
Paul Brar <ibrar@ualberta.ca>
2022-04-22 13:16:32 -0600

    Deploy React app to GitHub Pages

---
+++ precache-manifest.468947f0dd08a4e8025afc982f1b6e78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d81e2d20594664568fd5cea727867014",
+    "url": "/dEbay-Code/index.html"
+  },
+  {
+    "revision": "6efeb18584f9cff5ed8d",
+    "url": "/dEbay-Code/static/css/2.171a7357.chunk.css"
+  },
+  {
+    "revision": "728ba8522dd65e8865ac",
+    "url": "/dEbay-Code/static/css/main.23897544.chunk.css"
+  },
+  {
+    "revision": "6efeb18584f9cff5ed8d",
+    "url": "/dEbay-Code/static/js/2.dbfdcde9.chunk.js"
+  },
+  {
+    "revision": "25d4e673662a957c20aa4b3c4ab19362",
+    "url": "/dEbay-Code/static/js/2.dbfdcde9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "728ba8522dd65e8865ac",
+    "url": "/dEbay-Code/static/js/main.d420076f.chunk.js"
+  },
+  {
+    "revision": "88bd296708633965e1e8",
+    "url": "/dEbay-Code/static/js/runtime-main.a0e8f2cc.js"
+  }
+]);
